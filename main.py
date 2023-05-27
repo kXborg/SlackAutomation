bf8d62428c4042068e1ba62a64d58ff84a82b432
kXborg <kukilp213@gmail.com>
2023-05-27 18:10:34 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# import slack
+
+# # client = slack.WebClient(token = st.secrete["SLACK_TOKEN"])
+
+# # client.chat_postMessage(channel='#elen-commercial-and-production', link_names=1, text='@Kailash Das, this is an automated test message')
+
+# message = client.api_call('chat.postMessage',
+#                           channel='#elen-commercial-and-production',
+#                           text='This is a test')
